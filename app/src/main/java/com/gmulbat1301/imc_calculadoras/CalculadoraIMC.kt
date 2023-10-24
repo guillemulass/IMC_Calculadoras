127901066330f6ff1bb6398ce5cc27258af0d90c
Álvaro Castilla <acasloa946@g.educaand.es>
2023-10-24 21:28:48 +0200

    50% layout de IMC

--- app/src/main/java/com/gmulbat1301/imc_calculadoras/CalculadoraIMC.kt
+++ app/src/main/java/com/gmulbat1301/imc_calculadoras/CalculadoraIMC.kt
@@ -2,10 +2,24 @@ package com.gmulbat1301.imc_calculadoras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.slider.RangeSlider
+import com.google.android.material.slider.Slider
 
 class CalculadoraIMC : AppCompatActivity() {
+
+    lateinit var slider : RangeSlider
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora_imc)
+
+        initComponents()
+    }
+    fun initComponents(){
+        slider = findViewById<RangeSlider>(R.id.slider)
+        slider.valueFrom=120f
+        slider.valueTo=200f
+        slider.values = listOf(120f,200f)
+
     }
 }
+
