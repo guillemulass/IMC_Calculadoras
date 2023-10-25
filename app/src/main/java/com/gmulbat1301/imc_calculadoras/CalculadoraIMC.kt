9ed8fc043faf01f5536ddd6387c0666568a8cabb
Álvaro Castilla <acasloa946@g.educaand.es>
2023-10-25 02:10:18 +0200

    80% layout de IMC

--- app/src/main/java/com/gmulbat1301/imc_calculadoras/CalculadoraIMC.kt
+++ app/src/main/java/com/gmulbat1301/imc_calculadoras/CalculadoraIMC.kt
@@ -2,17 +2,22 @@ package com.gmulbat1301.imc_calculadoras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.slider.RangeSlider
 import com.google.android.material.slider.Slider
 
 class CalculadoraIMC : AppCompatActivity() {
 
     lateinit var slider : RangeSlider
+    lateinit var txtCm : TextView
+    lateinit var btnCalc : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora_imc)
 
         initComponents()
+        updatetxtCm()
     }
     fun initComponents(){
         slider = findViewById<RangeSlider>(R.id.slider)
@@ -20,6 +25,19 @@ class CalculadoraIMC : AppCompatActivity() {
         slider.valueTo=200f
         slider.values = listOf(120f,200f)
 
+        txtCm = findViewById<TextView>(R.id.stringcm)
+        btnCalc= findViewById(R.id.btn_Calc)
+    }
+
+    fun updatetxtCm() {
+        slider.addOnChangeListener { slider,value,fromUser ->
+            val valueText = String.format("%.1f - %.1f cm", slider.values[0], slider.values[1],)
+            txtCm.text = valueText
+        }
+    }
+
+    fun btnCalc() {
+        btnCalc.setOnClickListener { println() }
     }
 }
 
